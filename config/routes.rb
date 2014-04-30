7a06ba28e5424cac9fca4924871a6cbe6f58ad1c
Josh Dayan <thatoneguy150@gmail.com>
2014-04-29 23:42:15 -0500

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
-  
+  resources :users  
   root  'static_pages#hom'
   match '/signup',	to: 'users#new',			via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
